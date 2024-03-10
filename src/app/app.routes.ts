5e928e137673e54064b70b06d468f22706e566df
Alejandro Ajuria <alejoajuria@gmail.com>
2024-03-10 02:34:20 -0600

    login and signup components created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginComponent, SignupComponent } from './auth';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', title: 'Login', component: SignupComponent },
+  { path: 'login', title: 'Login', component: LoginComponent }
+];
